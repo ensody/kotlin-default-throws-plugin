49e26d22a0c7c96a4b64f27dbb34e30f49de9eed
Waldemar Kornewald <wkornewald@ensody.com>
2025-05-28 06:51:54 +0200

    Detach old stable releases

--- .github/autoupdate.py
+++ .github/autoupdate.py
@@ -1,60 +1,84 @@
 #!/usr/bin/env python3
 from utils import *
+from typing import List
 import sys
 import traceback
 
-# Collect the last max_stable stable releases and up to max_unstable unstable releases for which there is no
-# corresponding stable release yet.
-max_stable = 2
-max_unstable = 3
-releases = []
-stable_count = 0
-unstable_count = 0
-for release in request_json("https://api.github.com/repos/JetBrains/Kotlin/releases"):
-    kotlin_version = get_kotlin_version(release)
-    base_version = kotlin_version.split("-")[0].split("+")[0]
-    is_stable = "-" not in kotlin_version and "+" not in kotlin_version
-    if is_stable:
-        stable_count += 1
-    else:
-        if unstable_count >= max_unstable or any(get_kotlin_version(release).startswith(base_version) for release in releases) or has_version_tag(base_version):
-            continue
-        unstable_count += 1
-    releases.append(release)
-    if stable_count >= max_stable:
-        break
 
-errors = []
-for release in reversed(releases):
-    kotlin_version = get_kotlin_version(release)
-    if not has_version_tag(kotlin_version):
+def main():
+    all_versions = get_all_versions()
+
+    # Collect the last max_stable stable releases and up to max_unstable unstable releases for which there is no
+    # corresponding stable release yet.
+    max_stable = 2
+    max_unstable = 3
+    releases = []
+    stable_count = 0
+    unstable_count = 0
+    for release in request_json("https://api.github.com/repos/JetBrains/Kotlin/releases"):
+        kotlin_version = get_kotlin_version(release)
+        base_version = kotlin_version.split("-")[0].split("+")[0]
         is_stable = "-" not in kotlin_version and "+" not in kotlin_version
-        shell("git switch origin/main -C main")
-        if not is_stable:
-            shell("git switch --detach")
-        set_kotlin_version(kotlin_version)
-        latest_version = get_latest_version().rsplit("-plugin.", 1)[1]
-        plugin_version = f"{kotlin_version}-plugin.{latest_version}"
-        shell_extra_env["OVERRIDE_VERSION"] = plugin_version
-        try:
-            shell(f"./gradlew assemble --stacktrace")
-            shell(f"./gradlew testAll --stacktrace")
-        except Exception as e:
-            print(f"Error building for version {kotlin_version}:")
-            traceback.print_exception(e)
-            errors.append(e)
-            print("")
-
-        # Pushing a new tag will start a real publication in a separate CI workflow
-        shell(f"git add {VERSIONS_PATH}")
-        commit(f"Bumped to Kotlin {kotlin_version}", all_files=False)
-        tag(f"v-{plugin_version}")
-
         if is_stable:
-            push()
+            stable_count += 1
         else:
-            # Preview releases don't get pushed on main
-            shell(f"git push --tags")
+            if unstable_count >= max_unstable or \
+                any(get_kotlin_version(release).startswith(base_version)
+                    for release in releases) or \
+                has_version(base_version, all_versions):
+                continue
+            unstable_count += 1
+        releases.append(release)
+        if stable_count >= max_stable:
+            break
 
-if errors:
-    sys.exit(1)
+    errors = []
+    for release in reversed(releases):
+        kotlin_version = get_kotlin_version(release)
+        if not has_version(kotlin_version, all_versions):
+            is_stable = is_stable_version(kotlin_version)
+            # shell("git switch origin/main -C main")
+            kotlin_versions_with_plugin = set(version.rsplit("-plugin.", 1)[0] for version in all_versions)
+            if not is_stable or \
+                any(tuple(version.split(".")) > tuple(kotlin_version.split("."))
+                    for version in kotlin_versions_with_plugin if is_stable_version(version)):
+                shell("git switch --detach")
+            set_kotlin_version(kotlin_version)
+            latest_version = get_latest_version().rsplit("-plugin.", 1)[1]
+            plugin_version = f"{kotlin_version}-plugin.{latest_version}"
+            shell_extra_env["OVERRIDE_VERSION"] = plugin_version
+            try:
+                shell(f"./gradlew assemble --stacktrace")
+                shell(f"./gradlew testAll --stacktrace")
+            except KeyboardInterrupt:
+                pass
+            except Exception as e:
+                print(f"Error building for version {kotlin_version}:")
+                traceback.print_exc()
+                errors.append(e)
+                print("")
+
+            # Pushing a new tag will start a real publication in a separate CI workflow
+            shell(f"git add {VERSIONS_PATH}")
+            commit(f"Bumped to Kotlin {kotlin_version}", all_files=False)
+            git_tag(f"v-{plugin_version}")
+            all_versions.append(plugin_version)
+
+            if is_stable:
+                git_push()
+            else:
+                # Preview releases and old stable releases don't get pushed on main
+                git_push(tags_only=True)
+
+    if errors:
+        sys.exit(1)
+
+
+def has_version(kotlin_version: str, all_versions: List[str]) -> bool:
+    return any(version.startswith(f"{kotlin_version}-plugin.") for version in all_versions)
+
+def is_stable_version(version: str) -> bool:
+    return "-" not in version and "+" not in version
+
+if __name__ == "__main__":
+    main()
--- .github/utils.py
+++ .github/utils.py
@@ -1,5 +1,6 @@
-from subprocess import check_output, check_call
 from pathlib import Path
+from subprocess import check_output, check_call
+from typing import List
 from urllib.request import urlopen, Request
 import os, re, json
 
@@ -26,27 +27,31 @@ def request_json(url: str):
     return json.load(urlopen(Request(url, headers=headers)))
 
 def get_kotlin_version(json_data) -> str:
-    result = json_data["tag_name"].lstrip("v-")
+    result = get_version_from_tag(json_data["tag_name"])
     assert result
     return result
+
+def get_version_from_tag(tag: str) -> str:
+    return tag.lstrip("v-")
 
 def set_kotlin_version(kotlin_version: str):
     VERSIONS_PATH.write_text(version_re.sub(f"\\g<1>{kotlin_version}\\g<3>", VERSIONS_PATH.read_text()))
 
-def has_version_tag(kotlin_version: str) -> bool:
-    return shell_output(f"git tag --list 'v-{kotlin_version}-plugin.*'").strip()
+def get_all_versions() -> List[str]:
+    return [get_version_from_tag(tag) for tag in shell_output(f"git tag --list 'v-*'").strip().splitlines()]
 
 def get_latest_version() -> str:
-    return shell_output(f"git describe --tags --abbrev=0 --match 'v-*'").strip().lstrip("v-")
+    return get_version_from_tag(shell_output(f"git describe --tags --abbrev=0 --match 'v-*'").strip())
 
 def commit(message: str, all_files: bool = True):
     if all_files:
         shell(f"git add -A .")
     shell(f"git commit -m '{message}'")
 
-def tag(name: str):
+def git_tag(name: str):
     shell(f"git tag '{name}'")
 
-def push():
-    shell(f"git push")
+def git_push(tags_only: bool = False):
+    if not tags_only:
+        shell(f"git push")
     shell(f"git push --tags")
